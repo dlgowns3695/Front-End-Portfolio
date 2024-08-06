e0647e0caca85be4e9108479fa5af954c93b0b82
이해준 <kyo2263@naver.com>
2024-08-06 18:31:45 +0900

    svg test

--- 03/js/sideProgressBar.js
+++ 03/js/sideProgressBar.js
@@ -1,32 +1,53 @@
 
-function updateScroller() {
+// function updateScroller() {
     
+//     const scrollY = window.scrollY;
+//     const viewportHeight = window.innerHeight;
+//     const documentHeight = document.documentElement.scrollHeight;
+//     // console.log(documentHeight)
+
+//     const top = Math.round((scrollY / (documentHeight - viewportHeight)) * 100);
+//     const bottom = Math.round(
+//         ((scrollY + viewportHeight - documentHeight) /
+//         (documentHeight - viewportHeight)) *
+//         100
+//     );
+
+//     document.querySelector(".one").textContent = `${top} PSI`;
+//     document.querySelector(".two").textContent = `심해까지 ${bottom} M`;
+
+//     const rulerLines = document.querySelector('.rulerLines');
+//     // rulerLines.style.top = `${-documentHeight}px`;
+
+//     // console.log('리사이즈 및 스크롤함')
+    
+// }
+
+// window.addEventListener("scroll", updateScroller);
+// window.addEventListener("resize", updateScroller);
+
+// updateScroller();
+
+
+
+function updateScroller() {
     const scrollY = window.scrollY;
     const viewportHeight = window.innerHeight;
     const documentHeight = document.documentElement.scrollHeight;
-    console.log(documentHeight)
-
+  
     const top = Math.round((scrollY / (documentHeight - viewportHeight)) * 100);
     const bottom = Math.round(
-        ((scrollY + viewportHeight - documentHeight) /
+      ((scrollY + viewportHeight - documentHeight) /
         (documentHeight - viewportHeight)) *
         100
     );
-
+  
     document.querySelector(".one").textContent = `${top} PSI`;
     document.querySelector(".two").textContent = `심해까지 ${bottom} M`;
-
-    const rulerLines = document.querySelector('.rulerLines');
-    rulerLines.style.top = `${-documentHeight}px`;
-
-    // console.log('리사이즈 및 스크롤함')
+  
     
-}
-
-window.addEventListener("scroll", updateScroller);
-window.addEventListener("resize", updateScroller);
-
-updateScroller();
-
-
-
+  }
+  
+  window.addEventListener("scroll", updateScroller);
+  window.addEventListener("resize", updateScroller);
+  updateScroller();
