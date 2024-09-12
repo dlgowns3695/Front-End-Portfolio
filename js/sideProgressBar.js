70b45c3a398ba43760de1bdffc85e666687ac87a
이해준 <kyo2263@naver.com>
2024-09-12 17:13:09 +0900

    svg ++

--- js/sideProgressBar.js
+++ js/sideProgressBar.js
@@ -1,26 +1,4 @@
 
-// function updateScroller() {
-//     const scrollY = window.scrollY;
-//     const viewportHeight = window.innerHeight;
-//     const documentHeight = document.documentElement.scrollHeight;
-  
-//     const top = Math.round((scrollY / (documentHeight - viewportHeight)) * 100);
-//     const bottom = Math.round(
-//       ((scrollY + viewportHeight - documentHeight) /
-//         (documentHeight - viewportHeight)) *
-//         100
-//     );
-  
-//     document.querySelector(".psi").textContent = `${top} PSI`;
-//     document.querySelector(".miusM").textContent = `심해까지 ${bottom} M`;
-  
-    
-//   }
-  
-//   window.addEventListener("scroll", updateScroller);
-//   window.addEventListener("resize", updateScroller);
-//   updateScroller();
-
 function updateScroller() {
   const scrollY = window.scrollY;
   const viewportHeight = window.innerHeight;
@@ -41,6 +19,7 @@ function updateScroller() {
   // 스크롤 인디케이터의 위치 업데이트 (상단 0%, 하단 100% 가정)
   const rulerHeight = document.querySelector(".rulerLines").offsetHeight;
   const indicator = document.querySelector(".scroll-indicator");
+  console.log(indicator.offsetHeight)
 
   // 인디케이터의 위치를 스크롤 퍼센트에 맞게 조정
   const indicatorTop = (scrollY / (documentHeight - viewportHeight)) * (rulerHeight - indicator.offsetHeight);
